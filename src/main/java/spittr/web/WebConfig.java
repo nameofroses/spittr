94a47a2e1b39fe0c974f26a16da2f44fcd282d66
nameofroses <843665944@qq.com>
2024-09-24 21:22:57 +0800

    init project

---
+++ src/main/java/spittr/web/WebConfig.java
@@ -0,0 +1,35 @@
+package spittr.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "spitter")
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    // 视图解析器
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // TODO Auto-generated method stub
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:home");
+    }
+
+}
---
+++ src/test/java/spittr/SpittrApplicationTests.java
@@ -0,0 +1,13 @@
+package spittr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpittrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
